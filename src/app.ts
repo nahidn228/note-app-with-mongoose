505cfdb8547925d100939a235af4b40b259b4e21
nahidn228 <nahidn228@gmail.com>
2025-06-17 21:22:55 +0600

    creat read and update api create with mongoose and fix some middleware issues

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Application, Request, Response } from "express";
-import { model, Schema, Types } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const app: Application = express();
+app.use(express.json());
 
 const noteSchema = new Schema({
   title: { type: String, required: true, trim: true },
@@ -22,21 +23,58 @@ const noteSchema = new Schema({
 });
 const Note = model("Note", noteSchema);
 
-app.post("/create-note", async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Express",
-    content: "I am learning Mongoose with TS and Express",
-    tags: {
-      label: "Express",
-    },
-  });
+app.post("/notes/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
+  // Approach 1 creating a data
+  // const myNote = new Note({
+  //   title: "Learning Express",
+  //   content: "I am learning Mongoose with TS and Express",
+  //   tags: {
+  //     label: "Database",
+  //   },
+  // });
+  // await myNote.save();
 
-  await myNote.save();
+  // approach - 2
+  console.log(body);
+
+  const note = await Note.create(body);
 
   res.status(201).json({
     success: true,
     message: "note created successfully",
-    note: myNote,
+    note,
+  });
+});
+app.get("/notes", async (req: Request, res: Response) => {
+  const notes = await Note.find();
+
+  res.status(201).json({
+    success: true,
+    message: "note created successfully",
+    notes,
+  });
+});
+app.get("/note/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const notes = await Note.findById(id);
+
+  res.status(201).json({
+    success: true,
+    message: "note created successfully",
+    notes,
+  });
+});
+app.patch("/note/:id", async (req: Request, res: Response) => {
+  const noteId = req.params.id;
+  const updateBody = req.body;
+
+  const notes = await Note.findByIdAndUpdate(noteId, updateBody);
+
+  res.status(201).json({
+    success: true,
+    message: "note update successfully",
+    notes,
   });
 });
 
