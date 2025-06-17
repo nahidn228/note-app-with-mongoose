9a868aaff23b1a75b18f1ed0bb680c48fb36bc9f
nahidn228 <nahidn228@gmail.com>
2025-06-17 09:47:29 +0600

    server setup done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { Server } from "http";
+import app from "./app";
+
+let server: Server;
+const PORT = 5000;
+
+async function main() {
+  try {
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
