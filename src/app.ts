f45e57b85b7368f77c5f78b160072982a3f896af
nahidn228 <nahidn228@gmail.com>
2025-06-17 23:13:39 +0600

    User CRUD Operations and Making interface for Notes Schema

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 import { Note } from "./app/models/notes.models";
 import { notesRoutes } from "./app/controllers/notes.controller";
+import { usersRoutes } from "./app/controllers/user.controller";
 
 const app: Application = express();
 app.use(express.json());
-
 app.use("/notes", notesRoutes);
+app.use("/users", usersRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to note app");
